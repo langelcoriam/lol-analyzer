2b7d127723ca4bcce0226f2bf3948a31a5ade22f
langelcoriam <langel_coriam@outlook.com>
2026-08-15 21:48:08 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 #Lo que hacemos es importar y luego "as" abrevia como "st" en vez de poner streamlit
 import requests
+import base64
 
 RIOT_API_KEY= st.secrets["RIOT_API_KEY"]
 
@@ -12,6 +13,91 @@ st.set_page_config(
     page_icon="⚔️",
     layout="wide"
 )
+
+#Diccionario de hechizos de invocador
+
+hechizos = {
+    4: "Flash",
+    14: "Ignite",
+    12: "Teleport",
+    7: "Heal",
+    11: "Smite",
+    3: "Exhaust",
+    1: "Cleanse",
+    6: "Ghost",
+    21: "Barrier",
+}
+#Diccionario de Imangenes de hechizos de invocador
+
+imagenes_hechizos = {
+    4: "SummonerFlash",
+    14: "SummonerDot",
+    12: "SummonerTeleport",
+    7: "SummonerHeal",
+    6: "SummonerHaste",
+    3: "SummonerExhaust",
+    21: "SummonerBarrier",
+    1: "SummonerBoost",
+    11: "SummonerSmite"
+}
+
+import streamlit as st
+import requests
+import base64
+
+# ... API KEY ...
+def poner_fondo(imagen):
+    with open(imagen, "rb") as archivo:
+        imagen_base64 = base64.b64encode(archivo.read()).decode()
+
+    st.markdown(
+        f"""
+        <style>
+        .stApp {{
+            background-image:
+                linear-gradient(rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.45)),
+                url("data:image/jpg;base64,{imagen_base64}");
+            background-size: cover;
+            background-position: center;
+            background-attachment: fixed;
+        }}
+        .stApp p,
+        .stApp label,
+        .stApp h1,
+        .stApp h2,
+        .stApp h3,
+        .stApp h4 {{
+            color: white !important;
+        }}
+        
+        [data-testid="stSidebar"] p,
+        [data-testid="stSidebar"] span,
+        [data-testid="stSidebar"] label,
+        [data-testid="stSidebar"] h1,
+        [data-testid="stSidebar"] h2,
+        [data-testid="stSidebar"] h3,
+        [data-testid="stSidebar"] div {{
+            color: #222222 !important;
+        }}
+        
+        [class*="st-key-partida_"] {{
+            background: rgba(10, 10, 15, 0.58);
+            backdrop-filter: blur(7px);
+            -webkit-backdrop-filter: blur(7px);
+            border-radius: 14px;
+            padding: 18px;
+            margin-bottom: 14px;
+        }}        
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+poner_fondo("fondo.jpg")
+
+# AQUÍ CONTINÚA TODO TU CÓDIGO COMO YA LO TENÍAS
+
+
 #Set_page_config es literalemente como el estandar para la pagina digamos, como el titulo que aperece
 #el icono que sale en el navegador y el modo de vista Como "ultra Wide"
 st.title("LoL Analyzer")
@@ -137,18 +223,37 @@ elif seccion == "Partidas":
                     champion_name = jugador["championName"]
                     imagen_champion = f"https://ddragon.leagueoflegends.com/cdn/16.15.1/img/champion/{champion_name}.png"
                     
-                    with st.container():
-                        col1, col2, col3 = st.columns(3)
-                        
-                        col1.subheader(champion_name)
-                        col1.image(imagen_champion, width=80)
-
                     #DATOS BÁSICOS DEL JUGADOR
                     kills = jugador["kills"]
                     deaths = jugador["deaths"]
                     assists = jugador["assists"]
                     victoria = jugador["win"]
                     team_id = jugador["teamId"]
+                    
+                    #ITEMS
+                    items = [
+                        jugador["item0"],
+                        jugador["item1"],
+                        jugador["item2"],
+                        jugador["item3"],
+                        jugador["item4"],
+                        jugador["item5"],
+                        jugador["item6"],
+                    ]
+                    
+                    #HECHIZOS DE INVOCADOR
+                    hechizo1 = jugador["summoner1Id"]
+                    hechizo2 = jugador["summoner2Id"]
+                    #OBTENEMOS EL NOMBRE DEL HECHIZO USANDO EL DICCIONARIO DE HECHIZOS
+                    nombre_hechizo1 = hechizos.get(hechizo1, "Desconocido")
+                    nombre_hechizo2 = hechizos.get(hechizo2, "Desconocido")
+                    #OBTENER EL NOMBBRE DE LA IMAGEN DE HECHIZOS
+                    archivo_hechizo1 = imagenes_hechizos.get(hechizo1, "Desconocido")
+                    archivo_hechizo2 = imagenes_hechizos.get(hechizo2, "Desconocido")
+                    #URL DE LA IMAGEN DEL HECHIZO
+                    imagen_hechizo1 = f"https://ddragon.leagueoflegends.com/cdn/16.15.1/img/spell/{archivo_hechizo1}.png"
+                    imagen_hechizo2 = f"https://ddragon.leagueoflegends.com/cdn/16.15.1/img/spell/{archivo_hechizo2}.png"                   
+                    
 
                     #DURACIÓN DE LA PARTIDA
                     duracion = detalle_partida["info"]["gameDuration"]
@@ -160,7 +265,7 @@ elif seccion == "Partidas":
                     cs = minioms + monstruos
                     cs_por_minuto = round(cs / duracion_minutos, 2)
 
-                    #ORO
+                    #OROc
                     gold = jugador["goldEarned"]
                     gold_minuto = round(gold / duracion_minutos, 2)
 
@@ -192,27 +297,207 @@ elif seccion == "Partidas":
                     #============================================================
                     # MOSTRAR DATOS EN PANTALLA 
                     #============================================================
-                    with st.container():
-                        col1, col2, col3 = st.columns(3)
+                    with st.container(border=True,key=f"partida_{partida_id}"):
+                        col1, col2, col3= st.columns([1.2,2,2])
                         # Aquí usamos f-strings para combinar las variables de arriba en KDA, ORO, DAÑO ETC...
-                        col1.write(f"KDA: {kills} / {deaths} / {assists}")
-                        col1.write(f"CS: {cs}")
-                        col1.write(f"CS/min: {cs_por_minuto}")
-                        col1.write(f"Oro: {gold}")
-                        col1.write(f"Oro/min: {gold_minuto}")
-                        col1.write(f"Daño a campeones: {damage}")
-                        col2.write(f"Daño/min: {damage_minuto}")
-                        col2.write(f"Daño a torres: {damage_torres}")
-                        col2.write(f"Visión Score: {vision_score}")
-                        col2.write(f"Wards/min: {wards_minuto}")
-                        col2.write(f"Vision/min: {vision_minuto}")
-                        col3.write(f"Wards colocados: {wards}")
-                        col3.write(f"Kill Participation: {kp}%")
+                        
+                        #CAMPEÓN 
+                        with col1:
+                            st.markdown(
+                                f"<h3 style='margin-top: -17px; margin-bottom: 0px;'>{champion_name}</h3>",
+                                unsafe_allow_html=True
+                            )
+                            champ_col, spells_col = st.columns([1,1.5])
+                            
+                            with champ_col:
+                                st.image(imagen_champion, width=80)
+                            
+                            with spells_col:
+                                st.image(imagen_hechizo1, width=32)
+                                st.image(imagen_hechizo2, width=32)
+                            st.caption(f"Duración: {duracion_minutos} min")
+                            
+                            #===============================================
+                            #ITEMS Y RESULTADO
+                            #==============================================
+                            
+                            items_col, resultado_col = st.columns([4.5, 1.5])
+                            
+                            # ITEMS
+                            with items_col:
 
-                        if victoria:
-                            col3.success("resultado: Victoria")
-                        else:
-                            col3.error("resultado: Derrota")
+                                st.markdown(
+                                    """
+                                    <div style="
+                                        font-size: 13px;
+                                        font-weight: bold;
+                                        color: #ffffff;
+                                        margin-bottom: 6px;
+                                    ">
+                                        ITEMS:
+                                    </div>
+                                    """,
+                                    unsafe_allow_html=True
+                                )
+
+                                item_cols = st.columns(7)
+
+                                for i, item_id in enumerate(items):
+
+                                    with item_cols[i]:
+
+                                        if item_id != 0:
+
+                                            imagen_item = (
+                                                f"https://ddragon.leagueoflegends.com/"
+                                                f"cdn/16.15.1/img/item/{item_id}.png"
+                                            )
+
+                                            st.image(
+                                                imagen_item,
+                                                width=55
+                                            )
+                            
+                                
+                        
+                        #DATOS PRINCIPALES
+                        with col2:
+
+                            # KDA
+                            st.markdown(
+                                f"""
+                                <div style="
+                                    margin-bottom: 22px;
+                                    font-size: 17px;
+                                    font-weight: bold;
+                                    color: white;
+                                ">
+                                    <span style="color:#fbbf24; font-size:21px;">⚔</span>
+                                    &nbsp;&nbsp;KDA:
+                                    <span style="color:#4ade80;">{kills}</span> /
+                                    <span style="color:#f87171;">{deaths}</span> /
+                                    <span style="color:#60a5fa;">{assists}</span>
+                                </div>
+                                """,
+                                unsafe_allow_html=True
+                            )
+
+                            # CS
+                            st.markdown(
+                                f"""
+                                <div style="
+                                    margin-bottom: 22px;
+                                    font-size: 17px;
+                                    font-weight: bold;
+                                    color: white;
+                                ">
+                                    <span style="color:#c084fc; font-size:21px;">⚔</span>
+                                    &nbsp;&nbsp;CS: {cs}
+                                </div>
+                                """,
+                                unsafe_allow_html=True
+                            )
+
+                            # CS POR MINUTO
+                            st.markdown(
+                                f"""
+                                <div style="
+                                    margin-bottom: 22px;
+                                    font-size: 17px;
+                                    font-weight: bold;
+                                    color: white;
+                                ">
+                                    <span style="color:#60a5fa; font-size:21px;">◈</span>
+                                    &nbsp;&nbsp;CS/min: {cs_por_minuto}
+                                </div>
+                                """,
+                                unsafe_allow_html=True
+                            )
+
+                            # KILL PARTICIPATION
+                            st.markdown(
+                                f"""
+                                <div style="
+                                    font-size: 17px;
+                                    font-weight: bold;
+                                    color: white;
+                                ">
+                                    <span style="color:#4ade80; font-size:21px;">◎</span>
+                                    &nbsp;&nbsp;KP: {kp}%
+                                </div>
+                                """,
+                                unsafe_allow_html=True
+                            )
+                        
+                        # ECONOMIA Y DAÑO
+                        with col3:
+
+                            # ORO
+                            st.markdown(
+                                f"""
+                                <div style="
+                                    margin-bottom: 22px;
+                                    font-size: 17px;
+                                    font-weight: bold;
+                                    color: white;
+                                ">
+                                    <span style="color:#fbbf24; font-size:21px;">●</span>
+                                    &nbsp;&nbsp;Oro: {gold}
+                                </div>
+                                """,
+                                unsafe_allow_html=True
+                            )
+
+                            # ORO POR MINUTO
+                            st.markdown(
+                                f"""
+                                <div style="
+                                    margin-bottom: 22px;
+                                    font-size: 17px;
+                                    font-weight: bold;
+                                    color: white;
+                                ">
+                                    <span style="color:#f59e0b; font-size:21px;">◆</span>
+                                    &nbsp;&nbsp;Oro/min: {gold_minuto}
+                                </div>
+                                """,
+                                unsafe_allow_html=True
+                            )
+
+                            # DAÑO
+                            st.markdown(
+                                f"""
+                                <div style="
+                                    margin-bottom: 22px;
+                                    font-size: 17px;
+                                    font-weight: bold;
+                                    color: white;
+                                ">
+                                    <span style="color:#f87171; font-size:21px;">✦</span>
+                                    &nbsp;&nbsp;Daño: {damage}
+                                </div>
+                                """,
+                                unsafe_allow_html=True
+                            )
+
+                            # DAÑO POR MINUTO
+                            st.markdown(
+                                f"""
+                                <div style="
+                                    margin-bottom: 22px;
+                                    font-size: 17px;
+                                    font-weight: bold;
+                                    color: white;
+                                ">
+                                    <span style="color:#fb7185; font-size:21px;">✹</span>
+                                    &nbsp;&nbsp;Daño/min: {damage_minuto}
+                                </div>
+                                """,
+                                unsafe_allow_html=True
+                            )
+
+                                            
+                                
 
                     st.divider()
                     
@@ -226,3 +511,9 @@ elif seccion == "Timeline":
 #elif nos sirve como condicional, si "if" no se cumple compruba la siguiente si hicieramos 4 ifs 
 #comprobaria todos los resultados
 
+# ============================================================
+# INICIAR APP
+# ============================================================
+# En la terminal de VS Code:
+# cd "C:\Users\danel\OneDrive\Escritorio\Python\League Of Legends"
+# py -m streamlit run app.py
